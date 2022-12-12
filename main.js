8c933160a606cf4f5be7111b356e854d86ad33e7
Akshay Khona <DesertSled800c@gmail.com>
2022-12-12 10:08:18 -0600

    proj complete

---
+++ main.js
@@ -0,0 +1,9 @@
+const quarterValue = (num) => {
+    const quartered = num / 4
+    return quartered
+}
+
+// Invoke the function and store the return value
+let result = quarterValue(10)
+// Log the return value
+console.log(result);
